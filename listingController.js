85a00821e7027343c74954c0bb8bf2f64f806a1d
Brandon <bduong30@ufl.edu>
2016-01-15 13:21:11 -0500

    map

--- listingController.js
+++ listingController.js
@@ -3,6 +3,7 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
     $scope.sortType = "code";
+    $scope.markers = [];
 
     /* 
       Implement these functions in the controller to make your application function 
@@ -21,9 +22,46 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
         $scope.detailedInfo = undefined;
     };
       
-    $scope.showDetails = function(index) {
-        $scope.detailedInfo = $scope.listings[index];  
+
+	$scope.showDetails = function(index) {
+		$scope.detailedInfo = $scope.listings[index];
+		var selectedLatLng = {};
+
+		// if the listing has no supplied coordinates, set to ufs's center
+		if($scope.listings[index].coordinates == null){
+			selectedLatLng = {
+				lat:  29.643697, 
+				lng: -82.354992
+			};
+			map.setZoom(14);
+		}
+		else{
+			selectedLatLng = {
+				lat: Number($scope.detailedInfo.coordinates.latitude), 
+				lng: Number($scope.detailedInfo.coordinates.longitude)
+			};
+			map.setZoom(15);
+		}	
+		
+		// center map on the selected corrdinates
+		map.setCenter(new google.maps.LatLng(selectedLatLng.lat, selectedLatLng.lng));
+		
+
+		// clear old marker selections
+		for(var i = 0; i < $scope.markers.length; ++i){
+			$scope.markers[i].setMap(null);
+		}
+		
+		// create a new marker with selected coordinates
+		var new_marker = new google.maps.Marker({
+			position: selectedLatLng,
+			map: map,
+		 });
+
+		// add the new marker to the marker array
+		$scope.markers.push(new_marker);
     };
+
   }
 ]);
 
